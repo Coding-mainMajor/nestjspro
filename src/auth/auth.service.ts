366ed9498844820bbdf7d62f1645d6ef2a3e1faf
Chibueze <iwujichibueze7@gmail.com>
2025-07-14 06:40:08 +0100

    moduleSpecificConfig

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { UsersService } from 'src/users/users.service';
+import authConfig from './config/auth.config';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UsersService))
     private readonly userService: UsersService,
+    @Inject(authConfig.KEY)
+    private readonly authconfig: ConfigType<typeof authConfig>,
   ) {}
 
   isAuthenticated: Boolean = false;
 
   login(email: string, pswd: string) {
+    console.log(this.authconfig);
+    console.log(this.authconfig.sharedSecret);
     return 'user does not exist';
   }
 }
