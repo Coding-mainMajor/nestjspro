5e103e4a0c9c7cd50fd43f4bdb3ca29841a4226c
Chibueze <iwujichibueze7@gmail.com>
2025-07-17 02:40:26 +0100

    pgquery

--- src/common/pagination/pagination.provider.ts
+++ src/common/pagination/pagination.provider.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from './dto/pagination-query.dto';
-import { FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+import {
+  FindManyOptions,
+  FindOptionsWhere,
+  ObjectLiteral,
+  Repository,
+} from 'typeorm';
 
 @Injectable()
 export class PaginationProvider {
   public async paginationQuery<T extends ObjectLiteral>(
     paginationQueryDto: PaginationQueryDto,
     repository: Repository<T>,
-    where: FindOptionsWhere<T>
+    where: FindOptionsWhere<T>,
   ) {
-    return await repository.find({
+    const findOptions: FindManyOptions<T> = {
       skip: (paginationQueryDto.page! - 1) * paginationQueryDto.limit!,
       take: paginationQueryDto.limit,
-    });
+    };
+    if (where) {
+      findOptions.where = where;
+    }
+    return await repository.find(findOptions);
   }
 }
