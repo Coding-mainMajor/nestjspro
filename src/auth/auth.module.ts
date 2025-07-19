31e492cd7f2ac14afe6301b24f927858a7fa885d
Chibueze <iwujichibueze7@gmail.com>
2025-07-19 19:59:41 +0100

    jwt configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { HashingProvider } from './provider/hashing.provider';
 import { BcryptProvider } from './provider/bcrypt.provider';
 import authConfig from './config/auth.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -14,7 +15,11 @@ import authConfig from './config/auth.config';
     { provide: HashingProvider, useClass: BcryptProvider },
     BcryptProvider,
   ],
-  imports: [forwardRef(() => UsersModule), ConfigModule.forFeature(authConfig)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(authConfig),
+    JwtModule.registerAsync(authConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
